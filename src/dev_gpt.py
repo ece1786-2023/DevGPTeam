75ac5cd02e86cc0f60ffcb417562be6bc70c0431
alanyeung95 <45751387+alanyeung95@users.noreply.github.com>
2023-11-18 17:21:02 -0500

    refactor: Updsate dev GPT prompt

--- src/dev_gpt.py
+++ src/dev_gpt.py
@@ -12,14 +12,15 @@ Role: You are a professional software engineer; the main goal is to write PEP8 c
 
 Write code with triple quoto, based on the following list and context.
 
-1. You may output more than one file, but please use '<FILE_START>' and '<FILE_END>' tags to seperete them.
-2. You code must be able to be run on end-to-end
+1. You may output more than one file, but please use '<FILE_START>' and '<FILE_END>' tags to separate them.
+2. Your code must be able to be run end-to-end
 3. IMPORTANT: Put your generated project folder name between '<PROJECT_NAME_START>' and '<PROJECT_NAME_END>' tags.
-4. Replace 'GENERATED_FILE_NAME' with file name you propose.
-5. Please assume user will run the program by running main.py, so put the main class inside main.py
-6. The total number line of code generated should be less than 500
-7. Attention1: ALWAYS SET A DEFAULT VALUE, ALWAYS USE EXPLICIT VARIABLE.
-8. IMPORTANT: Please implement complete code snippets.
+4. Replace 'GENERATED_FILE_NAME' with the file name you propose.
+5. Please assume the user will run the program by running main.py, so put the main class inside main.py.
+6. Please assume all the graphic will be in pixel, avoid using extra assets like .png, .wav files.
+7. The total number of lines of code generated should be less than 500.
+8. Attention1: ALWAYS SET A DEFAULT VALUE, ALWAYS USE EXPLICIT VARIABLE.
+9. IMPORTANT: Please implement complete code snippets.
 
 Format Example:
 -----
@@ -42,6 +43,7 @@ additional_requirement="""
 Additional requirement:
 1. IMPORTANT: Put your generated project folder name between '<PROJECT_NAME_START>' and '<PROJECT_NAME_END>' tags.
 2. Output format strictly follow "Format example" in your context or instruction.
+3. Please assume all the graphic will be in pixel, avoid using extra assets like .png, .wav files.
 """
 
 
@@ -91,7 +93,7 @@ def generate_code(refined_requirement):
 
     print(refined_requirement + "\n" + additional_requirement)
 
-    response = utilities.call_openai_api_DEV(DEV_GPT_SYSTEM_CONTEXT_V3, refined_requirement + "\n" + additional_requirement, 0.1, 0.1)
+    response = utilities.call_openai_api_DEV(DEV_GPT_SYSTEM_CONTEXT_V3, refined_requirement + "\n" + additional_requirement, 0.3, 0.3)
     code_string = response.choices[0].message.content
     
     print(response.choices[0].message.content)
