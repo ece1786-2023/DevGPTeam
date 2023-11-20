6457fbdd82a4eca8a826d9f155e98075cab5f631
TianxiangGuo <tianxiang.guo@mail.utoronto.ca>
2023-11-20 14:12:27 -0500

    wip: Adding QA GPT code review

--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
 import pm_gpt
 import dev_gpt
 import qa_gpt
+import sys
 
 def main():
+    use_qa = True
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "--qa":
+            use_qa = True
+            print("Using QA")
+        else:
+            print("Unknown argument")
+    
     print("Welcome to Dev GPTeam!")
     initial_requirement = input("Please enter your initial software requirement: ")
     
@@ -39,7 +48,9 @@ def main():
 # 10. Exclude power-ups, bonuses, and additional features to maintain a classic gameplay experience.
 # ```"""
     generated_code = dev_gpt.generate_code(refined_requirement)
-    #qa_gpt.validate_code(generated_code)
+    if use_qa:
+        # qa_gpt.validate_code(generated_code)
+        qa_gpt.code_review(generated_code)
 
     print("Code generation completed!!!")
 
--- src/utilities.py
+++ src/utilities.py
@@ -29,10 +29,28 @@ def call_openai_api_DEV(system_context, user_prompt, top_p, temperature=1, model
 def call_openai_api_PM(messages, top_p=0.5, temperature=0.7, model="gpt-3.5-turbo-1106"):
     response = client.chat.completions.create(
         model=model,
-        #model="gpt-4-1106-preview",
         messages=messages,
         top_p=top_p,
         temperature=temperature
     )
     return response.choices[0].message.content
     # return response
+
+def call_openai_api_QA(messages, top_p=0.1, temperature=0.1, model="gpt-3.5-turbo-1106"):
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        top_p=top_p,
+        temperature=temperature
+    )
+    ret_response = response.choices[0].message.content
+    while response.choices[0].finish_reason != "stop":
+        msg = messages.append({"role":"assistant", "content":response})
+        response = client.chat.completions.create(
+            model=model,
+            messages=msg,
+            top_p=top_p,
+            temperature=temperature
+        )
+        ret_response += response.choices[0].message.content
+    return ret_response
