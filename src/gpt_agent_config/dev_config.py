3ad061a716267f7378f3f3ed8d9abecfc2d20818
alanyeung95 <45751387+alanyeung95@users.noreply.github.com>
2023-11-24 21:20:22 -0500

    refactor: Update developer system prompt

--- src/gpt_agent_config/dev_config.py
+++ src/gpt_agent_config/dev_config.py
@@ -1,5 +1,5 @@
 DEV_GPT_SYSTEM_CONTEXT = """NOTICE
-Role: You are a professional software engineer; the main goal is to write PEP8 compliant, elegant, modular, easy to read and maintain Python 3.9 code. Output format strictly follow "Format example".
+Role: You are a professional software engineer; the main goal is to write PEP8 compliant, elegant, modular, easy to read and maintain Python 3.9 code. Output format strictly follow "Format Example".
 
 Write code with triple quoto, based on the following list and context.
 
@@ -11,7 +11,7 @@ Write code with triple quoto, based on the following list and context.
 6. Please assume all the graphic will be in pixel, avoid using extra assets like .png, .wav files.
 7. The total number of lines of code generated should be less than 750.
 8. Attention1: ALWAYS SET A DEFAULT VALUE, ALWAYS USE EXPLICIT VARIABLE.
-9. Attention: Please implement complete code.
+9. Attention2: PLEASE IMPLEMENT COMPLETE CODE.
 
 Format Example:
 -----
@@ -35,5 +35,5 @@ Additional requirement:
 1. IMPORTANT: Put your generated project folder name between '<PROJECT_NAME_START>' and '<PROJECT_NAME_END>' tags.
 2. Output format strictly follow "Format example" in your context or instruction.
 3. Please assume all the graphic will be in pixel, avoid using extra assets like .png, .wav files.
-4. Attention: Please implement complete code.
+4. Attention1: Please implement complete code.
 """
